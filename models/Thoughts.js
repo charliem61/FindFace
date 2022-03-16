6093d089ba828e72a822a5b57c3dc0bb1db7fd20
Mason Walker <masonwalker30@gmail.com>
2022-03-15 22:05:38 -0700

    userJs and thoughtsJs started

---
+++ models/Thoughts.js
@@ -0,0 +1,18 @@
+// the same as user except for reaction because it  is a sub doc.
+//in class code in mini projects
+
+const assignmentSchema = require('./Assignment');
+
+
+
+
+
+
+
+
+
+
+
+assignments: [assignmentSchema],
+
+// reaction schema will look just like assignment.js file in 
